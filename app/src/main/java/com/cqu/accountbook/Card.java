0446c8b401090bb38fea2c4153029089003a105d
wuliao <1182300246@qq.com>
2022-06-13 10:18:36 +0800

    Initial commit

---
+++ app/src/main/java/com/cqu/accountbook/Card.java
@@ -0,0 +1,19 @@
+package com.cqu.accountbook;
+
+public class Card {
+    private final String name;
+    private final int imageId;
+
+    public Card(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
